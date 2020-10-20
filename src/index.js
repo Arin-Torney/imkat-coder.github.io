dc66506b74680cc58bb9ad2812b18d64f13f5451
IMKAT Coders <imkat.coders@gmail.com>
2020-10-20 20:46:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ let name = prompt("Enter your name");
 let username = prompt("Enter your mail id");
 let password = prompt("Enter a strong password");
 alert("Everything is set! But your account is for temporary use only for now.\nThat is, next time you open this website, you again need to sign up.");
-document.getElementById("account").innerHTML="<span class='w3-text-blue' style='text-align:right'><b>"name"</b></span>";
+document.getElementById("account").innerHTML="<span class='w3-text-blue' style='text-align:right' text=name><b></b></span>";
 }
